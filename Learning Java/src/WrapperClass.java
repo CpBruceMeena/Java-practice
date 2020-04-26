880b3626bdd523513f6ee839d5b829f33a7dc7ec
CpBruceMeena <sandeep10198@gmail.com>
2020-04-27 02:14:29 +0530

    learning wrapper class

--- Learning Java/src/WrapperClass.java
+++ Learning Java/src/WrapperClass.java
@@ -2,8 +2,15 @@
 public class WrapperClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Integer i1 = Integer.valueOf("125");  // it is an static method
+		Float f1 = Float.valueOf("235.25"); // it's also an static method
+		int i = Integer.parseInt("235235");  // It is an static method
+		int c = i1.intValue();   // it's an instance based function
+				// means we have to use an object to call the function
+			
+		System.out.print(i);
+		System.out.print(f1);
+		System.out.print(c);
 	}
 
 }
